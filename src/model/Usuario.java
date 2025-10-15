6059964b6f001199bc300f971ebfa2f303fb7873
Bernardo Pierobon <85205657+beritera@users.noreply.github.com>
2025-10-14 21:59:48 -0300

    Add files via upload

---
+++ src/model/Usuario.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Usuario { 
+
+    private int id;
+    private String login;
+    private String senha;
+    private String dataCriacao;
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getLogin() { return login; }
+    public void setLogin(String login) { this.login = login; }
+
+    public String getSenha() { return senha; }
+    public void getSenha(String senha) { this.senha = senha; }
+
+    public String getDataCriacao() { return dataCriacao; }
+    public void getdataCriacao(String dataCriacao) { this.dataCriacao = dataCriacao; }
+}
