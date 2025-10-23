ada61bb77fee27da7fbccf88dbefc341dedd2a8c
Bernardo Pierobon <85205657+beritera@users.noreply.github.com>
2025-10-23 20:24:34 -0300

    Update script.sql

--- src/dao/script.sql
+++ src/dao/script.sql
@@ -3,7 +3,14 @@ CREATE DATABASE univel4a;
 USE univel4a;
 
 CREATE TABLE usuarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+	id INT auto_increment PRIMARY KEY ,
     nome_usuario VARCHAR(50) NOT NULL,
-    data_criacao TIMESTAMP
+    senha_hash VARCHAR(64) NOT NULL,
+    data_criacao DATETIME DEFAULT CURRENT_TIMESTAMP,
+    tipo VARCHAR(1) CHECK (tipo IN ('U', 'A')) DEFAULT 'U',
+    ativo VARCHAR(1) CHECK (ativo IN ('T', 'F')) DEFAULT 'T'
+);
+
+INSERT INTO usuarios VALUES (
+	2, 'bernardo', 'pierobon', current_date(), 'A', 'T'
 );
