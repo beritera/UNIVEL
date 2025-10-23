4172907983301a61d986d48ae58ea710ff909aa8
Bernardo Pierobon <85205657+beritera@users.noreply.github.com>
2025-10-23 20:24:47 -0300

    Update Usuario.java

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -6,6 +6,8 @@ public class Usuario {
     private String login;
     private String senha;
     private String dataCriacao;
+    private String ativo;
+    private String tipo;
 
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
@@ -18,4 +20,10 @@ public class Usuario {
 
     public String getDataCriacao() { return dataCriacao; }
     public void getdataCriacao(String dataCriacao) { this.dataCriacao = dataCriacao; }
+
+    public String getTipo() { return tipo; }
+    public void setTipo(String tipo) { this.tipo = tipo; }
+
+    public String getAtivo() { return ativo; }
+    public void setAtivo(String ativo) { this.ativo = ativo; }
 }
